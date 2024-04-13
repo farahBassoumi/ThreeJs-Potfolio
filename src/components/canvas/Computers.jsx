b8f421c89d389aa5425c91bbd7ab0038626a2683
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-14 00:25:25 +0100

    writing it explicitely

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import CanvasLoader from "../Loader";
 
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF("../../../desktop_pc/scene.gltf");
+  const computer = useGLTF("/ThreeJs-Potfolio/public/desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={1} groundColor="black" />
